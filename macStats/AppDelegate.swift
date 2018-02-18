6dc9c3d53e6e2115f9a1019f76d33cc1d9451cae
Mark Wales <mark@smallhadroncollider.com>
2018-02-18 11:09:32 +0000

    Initial Commit

---
+++ macStats/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macStats
+//
+//  Created by Mark Wales on 18/02/2018.
+//  Copyright © 2018 Mark Wales. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macStats/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macStats
+//
+//  Created by Mark Wales on 18/02/2018.
+//  Copyright © 2018 Mark Wales. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
